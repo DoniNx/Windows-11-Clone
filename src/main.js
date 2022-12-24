c18d9f333b8b41633d735907f7aa24f666c51693
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2022-12-24 10:45:21 +0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+import './style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faWifi, faBatteryEmpty, faVolumeLow, faBell, faAngleUp, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faMagnifyingGlass, faWifi, faBatteryEmpty, faVolumeLow, faBell, faAngleUp)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
+
+
+
+
+
+
